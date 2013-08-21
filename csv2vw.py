85786aaa5c7094b3ec2e00480a92b7d38fa1fea6
jofusa <dennison.john@gmail.com>
2013-08-21 10:18:18 -0600

    addded tabs back in

--- csv2vw.py
+++ csv2vw.py
@@ -6,28 +6,28 @@ import csv
 import argparse
 
 def construct_line( label, line ):
-    new_line = []
-    try:
-        label = float( label )
-    except Exception, e:
-        pass
+	new_line = []
+	try:
+		label = float( label )
+	except Exception, e:
+		pass
 
-    if label == 0.0:
-        label = "0"
-    new_line.append( "%s |n " % ( label ))
+	if label == 0.0:
+		label = "0"
+	new_line.append( "%s |n " % ( label ))
 
-    for i, item in enumerate( line ):
-        try:
-            item = float( item )
-        except ValueError, e:
-            pass
-        if item == 0.0:
-            continue    # sparse!!!
-        new_item = "%s:%s" % ( i + 1, item )
-        new_line.append( new_item )
-    new_line = " ".join( new_line )
-    new_line += "\n"
-    return new_line
+	for i, item in enumerate( line ):
+		try:
+			item = float( item )
+		except ValueError, e:
+			pass
+		if item == 0.0:
+			continue    # sparse!!!
+		new_item = "%s:%s" % ( i + 1, item )
+		new_line.append( new_item )
+	new_line = " ".join( new_line )
+	new_line += "\n"
+	return new_line
 
 # ---
 
@@ -37,10 +37,10 @@ parser.add_argument("output_file", help = "path to output file")
 
 
 parser.add_argument("-l", "--label_index", help = "specify index of label col", type=int,
-                    default = -1)
+					default = -1)
 
 parser.add_argument("-s", "--skip_headers", help = "specify if there are headers in the file - default false",
-                     action="store_true")
+					 action="store_true")
 
 args = parser.parse_args()
 
@@ -53,21 +53,21 @@ o = open( output_file, 'w' )
 
 reader = csv.reader( i )
 if args.skip_headers:
-    headers = reader.next()
+	headers = reader.next()
 
 n = 0
 
 for line in reader:
-    if args.label_index == -1:
-        label = 1
-    else:
-        label = line.pop( args.label_index )
+	if args.label_index == -1:
+		label = 1
+	else:
+		label = line.pop( args.label_index )
 
-    new_line = construct_line( label, line )
-    o.write( new_line )
+	new_line = construct_line( label, line )
+	o.write( new_line )
 
-    n += 1
-    if n % 10000 == 0:
-        print n
+	n += 1
+	if n % 10000 == 0:
+		print n
 
 
